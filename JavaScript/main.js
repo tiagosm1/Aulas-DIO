3e1133937ddd8080f446e256df92356faa25edda
TIAGO MOREIRA <72826762+tiagosm1@users.noreply.github.com>
2021-03-22 17:19:21 -0300

    js_dio

---
+++ JavaScript/main.js
@@ -0,0 +1,15 @@
+/*var nome = "Tiago Moreira";
+var idade = 31;
+var idade2 = 10;
+var frase = "SPFC é o melhor time do mundo"
+//alert("Bem vindo " + nome + " você tem " + idade + " anos de idade")
+alert(idade + idade2);
+console.log(nome)
+console.log(idade+idade2)
+alert(frase.toUpperCase())*/
+//console.log(frase.replace("SPFC", "Juventus Mooca"))
+var lista = ["maça", "pera", "laranja"];
+lista.push("Uva");//coloca elemento no array
+alert(lista);
+lista.pop("pera");//tira elemento do array
+alert(lista);
